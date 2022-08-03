28c51efe537cd7efdb84190963999f8a41061e89
Ivan <i.zashchyk@gmail.com>
2022-08-03 17:10:10 +0300

    ...

--- start.py
+++ start.py
@@ -2,7 +2,7 @@ import telepot
 
 token = '5414010081:AAFHzRIjpEJK6UOVn7h_N7xKOMrXMUjxA8Q'
 bot = telepot.Bot(token)
-address = 'https://doc-converter-bot.herokuapp.com/'
+address = 'https://doc-converter-bot.herokuapp.com'
 
 bot.deleteWebhook()
 bot.setWebhook(f'{address}/oldman')
