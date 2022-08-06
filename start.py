82f31c57ca08ae550359552bff26148d346e5efd
Ivan <i.zashchyk@gmail.com>
2022-08-06 15:51:43 +0300

    i guess all is ok

--- start.py
+++ start.py
@@ -2,7 +2,7 @@ import telepot
 
 token = '5414010081:AAFHzRIjpEJK6UOVn7h_N7xKOMrXMUjxA8Q'
 bot = telepot.Bot(token)
-address = 'https://a94b-176-98-31-129.ngrok.io'
+address = 'https://9503-176-98-31-129.ngrok.io'
 
 bot.deleteWebhook()
 bot.setWebhook(f'{address}/oldman')
