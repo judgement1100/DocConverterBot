79e1e31aad69c2f254be14099045d79a3911042b
Ivan <i.zashchyk@gmail.com>
2022-08-06 14:16:44 +0300

    another commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='DocConverterBot',
+    version='',
+    packages=['mysite', 'mysite.setup', 'mysite.tgbot', 'mysite.tgbot.migrations'],
+    url='',
+    license='',
+    author='Admin',
+    author_email='',
+    description=''
+)
