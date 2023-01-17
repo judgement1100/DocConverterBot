d7bdea6c332613dfc37ba1be510f11f657709203
Ivan <i.zashchyk@gmail.com>
2023-01-17 16:23:18 +0200

    Modified .gitignore: pasted code from internet, added start.py

--- start.py
+++ start.py
@@ -4,6 +4,7 @@ import telepot
 token = '5414010081:AAFHzRIjpEJK6UOVn7h_N7xKOMrXMUjxA8Q'
 bot = telepot.Bot(token)
 address = 'https://2cc6-176-98-8-129.ngrok.io'
+# asdfasf
 
 bot.deleteWebhook()
 bot.setWebhook(f'{address}/oldman')
